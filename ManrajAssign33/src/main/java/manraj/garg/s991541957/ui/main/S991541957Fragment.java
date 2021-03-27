557c97d6432a318a9ce40007671f17e9a14b7386
Manraj <manraj_g93@hotmail.com>
2021-03-26 22:15:10 -0400

    Added anim folder with rotation animation code to rotate textviews 45 degrees Added 2 textviews as per assignmnet requirements and the associated code to rotate them

--- ManrajAssign33/src/main/java/manraj/garg/s991541957/ui/main/S991541957Fragment.java
+++ ManrajAssign33/src/main/java/manraj/garg/s991541957/ui/main/S991541957Fragment.java
@@ -1,5 +1,6 @@
 package manraj.garg.s991541957.ui.main;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,6 +9,11 @@ import androidx.lifecycle.ViewModelProviders;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.RotateAnimation;
+import android.widget.Button;
+import android.widget.TextView;
 
 import manraj.garg.s991541957.R;
 
@@ -17,6 +23,11 @@ import manraj.garg.s991541957.R;
  * create an instance of this fragment.
  */
 public class S991541957Fragment extends Fragment {
+
+    private TextView firstName;
+    private TextView lastName;
+    private Animation rotation;
+    Context context;
 
     private static final String TAG = "S991541957";
 
@@ -40,6 +51,15 @@ public class S991541957Fragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_s991541957, container, false);
+        context = container.getContext();
+        View root = inflater.inflate(R.layout.fragment_s991541957, container, false);
+        firstName = (TextView) root.findViewById(R.id.manrajFirstName);
+        lastName = (TextView) root.findViewById(R.id.manrajLastName);
+
+        rotation = AnimationUtils.loadAnimation(context, R.anim.rotator);
+        firstName.startAnimation(rotation);
+        lastName.startAnimation(rotation);
+
+        return root;
     }
 }
